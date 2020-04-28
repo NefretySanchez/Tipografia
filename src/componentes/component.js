69bae9a99bda5300624bf403ed71ac35f1e6e1fc
NefretySanchez <57744620+NefretySanchez@users.noreply.github.com>
2020-04-27 23:56:39 -0500

    Create component.js

---
+++ src/componentes/component.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/style.css";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <p className="letra">Nefrety</p>
+        <p className="nombre">Sanchez</p>
+      </div>
+    );
+  }
+}
+
+export default Layout;
